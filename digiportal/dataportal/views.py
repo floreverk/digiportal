41dbf1e3e116febceda7eeb3118d7a4bb973056f
floreverk <floreverkest1@gmail.com>
2024-04-12 15:31:12 +0200

    .

--- digiportal/dataportal/views.py
+++ digiportal/dataportal/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .quality import iffanalyse
+from .quality import iffanalyse, iffimage
 from .statistics import iffgraph
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
@@ -134,3 +134,145 @@ def iffforms(request):
     else:
         form = bruikleenform()
         return render(request, 'iffforms.html', {'form': form})
+	
+def iffimage(request):
+	response = HttpResponse(content_type='application/ms-excel')
+	response['Content-Disposition'] = 'attachment; filename="#000.xlsx"'
+	wb = Workbook()
+	ws = wb.active
+	ws.title = 'Info'
+	ws['A1'] = "list of sheet tab codes"
+	ws.append(['sheet number', 'quality check'])
+	ws.append(['#001', 'records adlib te digitaliseren'])
+	ws.append(['#002', 'objecten adlib te digitaliseren'])
+	ws.append(['#003', 'fotos adlib te digitaliseren'])
+	ws.append(['#004', 'documenten adlib te digitaliseren'])
+	ws.append(['#005', 'records adlib, afbeelding te koppelen'])
+	ws.append(['#006', 'beelden te registreren in adlib'])
+	ws.append(['#007', 'HR beeld te maken van RAW'])
+	ws.append(['#008', 'afgeleide te maken van HR/RAW beeld'])
+	ws.append(['#009', 'geen HR/RAW beeld beschikbaar'])
+	ws.append(['#010', 'HR < 300dpi'])
+	ws.append(['#011', 'LR > 72dpi'])
+	ws.append(['#012', '.tif beelden in LR'])
+	ws.append(['#013', 'dubbele beelden'])
+	df_001 = iffimage.iffi_001()
+	df_002 = iffimage.iffi_002()
+	df_003 = iffimage.iffi_003()
+	df_004 = iffimage.iffi_004()
+	df_005 = iffimage.iffi_005()
+	df_006 = iffimage.iffi_006()
+	df_007 = iffimage.iffi_007()
+	df_008 = iffimage.iffi_008()
+	df_009 = iffimage.iffi_009()
+	df_010 = iffimage.iffi_010()
+	df_011 = iffimage.iffi_011()
+	df_012 = iffimage.iffi_012()
+	df_013 = iffimage.iffi_013()
+
+	if df_001.empty == True:
+		print('empty dataframe')
+	else:
+		ws = wb.create_sheet("#001")
+		rows = dataframe_to_rows(df_001, index=False)
+		for r_idx, row in enumerate(rows, 1):
+			for c_idx, value in enumerate(row, 1):
+				ws.cell(row=r_idx, column=c_idx, value=value)
+	if df_002.empty == True:
+		print('empty dataframe')
+	else:
+		ws = wb.create_sheet("#002")
+		rows = dataframe_to_rows(df_002, index=False)
+		for r_idx, row in enumerate(rows, 1):
+			for c_idx, value in enumerate(row, 1):
+				ws.cell(row=r_idx, column=c_idx, value=value)
+	if df_003.empty == True:
+		print('empty dataframe')
+	else:
+		ws = wb.create_sheet("#003")
+		rows = dataframe_to_rows(df_003, index=False)
+		for r_idx, row in enumerate(rows, 1):
+			for c_idx, value in enumerate(row, 1):
+				ws.cell(row=r_idx, column=c_idx, value=value)
+	if df_004.empty == True:
+		print('empty dataframe')
+	else:
+		ws = wb.create_sheet("#004")
+		rows = dataframe_to_rows(df_004, index=False)
+		for r_idx, row in enumerate(rows, 1):
+			for c_idx, value in enumerate(row, 1):
+				ws.cell(row=r_idx, column=c_idx, value=value)
+	if df_005.empty == True:
+		print('empty dataframe')
+	else:
+		ws = wb.create_sheet("#005")
+		rows = dataframe_to_rows(df_005, index=False)
+		for r_idx, row in enumerate(rows, 1):
+			for c_idx, value in enumerate(row, 1):
+				ws.cell(row=r_idx, column=c_idx, value=value)
+	if df_006.empty == True:
+		print('empty dataframe')
+	else:
+		ws = wb.create_sheet("#006")
+		rows = dataframe_to_rows(df_006, index=False)
+		for r_idx, row in enumerate(rows, 1):
+			for c_idx, value in enumerate(row, 1):
+				ws.cell(row=r_idx, column=c_idx, value=value)
+	if df_007.empty == True:
+		print('empty dataframe')
+	else:
+		ws = wb.create_sheet("#007")
+		rows = dataframe_to_rows(df_007, index=False)
+		for r_idx, row in enumerate(rows, 1):
+			for c_idx, value in enumerate(row, 1):
+				ws.cell(row=r_idx, column=c_idx, value=value)
+	if df_008.empty == True:
+		print('empty dataframe')
+	else:
+		ws = wb.create_sheet("#008")
+		rows = dataframe_to_rows(df_008, index=False)
+		for r_idx, row in enumerate(rows, 1):
+			for c_idx, value in enumerate(row, 1):
+				ws.cell(row=r_idx, column=c_idx, value=value)
+	if df_009.empty == True:
+		print('empty dataframe')
+	else:
+		ws = wb.create_sheet("#009")
+		rows = dataframe_to_rows(df_009, index=False)
+		for r_idx, row in enumerate(rows, 1):
+			for c_idx, value in enumerate(row, 1):
+				ws.cell(row=r_idx, column=c_idx, value=value)
+	if df_010.empty == True:
+		print('empty dataframe')
+	else:
+		ws = wb.create_sheet("#010")
+		rows = dataframe_to_rows(df_010, index=False)
+		for r_idx, row in enumerate(rows, 1):
+			for c_idx, value in enumerate(row, 1):
+				ws.cell(row=r_idx, column=c_idx, value=value)
+	if df_011.empty == True:
+		print('empty dataframe')
+	else:
+		ws = wb.create_sheet("#011")
+		rows = dataframe_to_rows(df_011, index=False)
+		for r_idx, row in enumerate(rows, 1):
+			for c_idx, value in enumerate(row, 1):
+				ws.cell(row=r_idx, column=c_idx, value=value)
+	if df_012.empty == True:
+		print('empty dataframe')
+	else:
+		ws = wb.create_sheet("#012")
+		rows = dataframe_to_rows(df_012, index=False)
+		for r_idx, row in enumerate(rows, 1):
+			for c_idx, value in enumerate(row, 1):
+				ws.cell(row=r_idx, column=c_idx, value=value)
+	if df_013.empty == True:
+		print('empty dataframe')
+	else:
+		ws = wb.create_sheet("#013")
+		rows = dataframe_to_rows(df_013, index=False)
+		for r_idx, row in enumerate(rows, 1):
+			for c_idx, value in enumerate(row, 1):
+				ws.cell(row=r_idx, column=c_idx, value=value)
+	wb.save(response)
+	return response
